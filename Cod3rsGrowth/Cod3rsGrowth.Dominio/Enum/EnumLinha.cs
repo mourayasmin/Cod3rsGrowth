a7fe3867befa6f615a9e27581603986d20a516b8
Yasmin Moura <yasmin.moura@discente.ufg.br>
2024-05-15 14:04:14 -0300

    alterações pedidas no pull request

---
+++ Cod3rsGrowth/Cod3rsGrowth.Dominio/Enum/EnumLinha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Cod3rsGrowth.Dominio.Enum
+{
+    public enum EnumLinha
+    {
+        Performance, 
+        Casual, 
+        Skate, 
+        Corrida
+    }
+}
---
+++ Cod3rsGrowth/Cod3rsGrowth.Dominio/Modelos/ClasseTenis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cod3rsGrowth.Cod3rsGrowth.Dominio.Enum
+
+namespace Cod3rsGrowth.Dominio
+{
+    public class Tenis
+    {
+        public EnumLinha Linha { get; set; }
+        public int Id { get; set; }
+        public int Idmarca { get; set; }
+        public double Preco { get; set; }
+        public DateTime Lancamento { get; set; }
+        public decimal Avaliacao { get; set; }
+        public string Nome { get; set; }
+        public bool Disponibilidade { get; set; }
+    }
+}
