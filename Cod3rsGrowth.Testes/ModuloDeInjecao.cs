e00f51d2a99b150c7f754ea262bf8563ca0f4a1b
Yasmin <yasmin.moura@discente.ufg.br>
2024-05-21 11:06:58 -0300

    arq

---
+++ Cod3rsGrowth.Testes/ModuloDeInjecao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cod3rsGrowth.Testes
+{
+    public class ModuloDeInjecao
+    {
+        public static ServiceCollection Servico { get; set; }
+        public ModuloDeInjecao() {
+            Servico = new ServiceCollection();
+            Servico.AddScoped<IRepositorioMock, RepositorioMock>();
+        }    
+        public static ServiceProvider BuildServiceProvider()
+        {
+            return Servico.BuildServiceProvider();
+        }
+    }
+}
