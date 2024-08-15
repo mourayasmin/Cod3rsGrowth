2f61b4a33459025912383ef0704670dd71012e78
Yasmin <yasmin.moura@discente.ufg.br>
2024-08-15 17:22:34 -0300

    botao press me retirado; adicionado titulo na pagina inicial

--- Cod3rsGrowth.Web/wwwroot/app/Marca/Marca.controller.js
+++ Cod3rsGrowth.Web/wwwroot/app/Marca/Marca.controller.js
@@ -3,16 +3,5 @@ sap.ui.define([
 ], function (BaseController) {
 	"use strict";
 	return BaseController.extend("ui5.wwwroot.app.Marca.Marca", {
-        onInit: function() {
-          
-            var that = this;
-            window.setTimeout(function() {
-                that.byId("pressMeButton").setVisible(true);
-            }, Math.random() * 10000);
-        },
-
-        onPress: function() {
-            this.byId("pressMeButton").setText("I got pressed");
-      }
 	});
 });
