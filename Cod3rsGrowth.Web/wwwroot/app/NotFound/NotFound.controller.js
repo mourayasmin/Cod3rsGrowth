69b79a2d85c6db542cfed824bf15d20875f4f8b8
Yasmin <yasmin.moura@discente.ufg.br>
2024-08-15 09:51:39 -0300

    implementando tela de not found; inicio do botao de voltar

---
+++ Cod3rsGrowth.Web/wwwroot/app/NotFound/NotFound.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/demo/nav/controller/BaseController"
+ ], function (BaseController) {
+    "use strict";
+    return BaseController.extend("ui5.wwwroot.app.NotFound.NotFound", {
+       onInit: function () {
+       }
+    });
+ });
